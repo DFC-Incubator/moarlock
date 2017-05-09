14db85a5a2d44cb48486b74681a54a1d28d72b21
michael-conway <michael.c.conway@gmail.com>
2017-05-09 09:28:53 -0400

    #1 skeleton main method

--- src/main/java/org/irods/jargon/moarlock/MoarlockServiceImpl.java
+++ src/main/java/org/irods/jargon/moarlock/MoarlockServiceImpl.java
@@ -6,6 +6,7 @@ package org.irods.jargon.moarlock;
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
+import org.irods.jargon.core.pub.IRODSFileSystem;
 import org.irods.jargon.core.service.AbstractJargonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,17 +44,57 @@ public class MoarlockServiceImpl extends AbstractJargonService {
 	}
 
 	private void stageMetadata() {
-		// TODO Auto-generated method stub
+		log.info("stageMetadata()");
 
 	}
 
 	private void markOutputWithTrackingData() {
-		// TODO Auto-generated method stub
+		log.info("markOutputWithTrackingData()");
 
 	}
 
 	private void stageOutputFiles() {
-		// TODO Auto-generated method stub
+		log.info("stageOutputFiles()");
+
+	}
+
+	/**
+	 * run processor
+	 * 
+	 * @param args
+	 * 
+	 * 
+	 *            <ul>
+	 *            <li>arg0=outputdir</li>
+	 *            <li>arg1=host</li>
+	 *            <li>arg2=port</li>
+	 *            <li>arg3=zone</li>
+	 *            <li>arg4=user</li>
+	 *            <li>arg5=password</li>
+	 *            <li>arg6=guid</li>
+	 *            <li>arg7=parm1</li>
+	 *            <li>arg8=parm2</li>
+	 *            <li>arg9=parm3</li>
+	 * 
+	 *            </ul>
+	 * 
+	 * 
+	 * @throws JargonException
+	 */
+	public static void main(final String[] args) throws JargonException {
+
+		if (args.length == 0) {
+			log.error("no args provided");
+			throw new JargonException("no args provided");
+		}
+
+		final AnalysisParams analysisParams = new AnalysisParams();
+		final IRODSAccount irodsAccount = null;
+		final IRODSFileSystem irodsFileSystem = IRODSFileSystem.instance();
+		// fill in with args
+		final MoarlockServiceImpl moarlockServiceImpl = new MoarlockServiceImpl(
+				irodsFileSystem.getIRODSAccessObjectFactory(), irodsAccount, analysisParams);
+		moarlockServiceImpl.processAnalysis();
 
 	}
 
